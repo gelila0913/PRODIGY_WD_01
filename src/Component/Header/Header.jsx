baa52829d5269b3f2b07776472a7b26374d0496a
gelila@0913 <gelilasintayehu79@gmail.com>
2026-03-30 21:32:06 +0300

    final version of the landing page

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import { Search,User } from "lucide-react";
-const Header =() =>{
-    return(
-        <>
-        <body>
-        <header className="header">
-            <div className="container">
-            <div className="logo">
-                <h2>JOYVIEW</h2>
-            </div>
-            <nav className="nav">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Shows</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="">MyList</a></li>
-                </ul>
-            </nav>
-           </div>
-           <div className="search">
-            <ul>
-            <li><Search className="icon1" size={18}/>             
-            <input type="text" placeholder="    search" /> </li>
-            <li>Kids</li>
+import { Search, User, Menu, X } from "lucide-react";
 
-            <li> <User className="icon" size={20} /> </li>
-            </ul>
+const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
-           </div>
-        </header>
-        </body>
-        </>
-    )
-}
+  return (
+    <header className="header">
+      <div className="header-left">
+        <div className="logo">
+          <h2>JOYVIEW</h2>
+        </div>
+        
+        {/* Toggle 'active' class based on state */}
+        <nav className={`nav ${isMobile ? "nav-active" : ""}`}>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Shows</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">MyList</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="header-right">
+        <div className="search-box">
+          <Search className="search-icon" size={18} />
+          <input type="text" placeholder="Search" />
+        </div>
+        
+        <ul className="user-nav">
+          <li className="kids-label">Kids</li>
+          <li><User className="icon" size={20} /></li>
+        </ul>
+
+        {/* Hamburger Icon */}
+        <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+          {isMobile ? <X size={30} /> : <Menu size={30} />}
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
